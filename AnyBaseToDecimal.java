8ee076b4abf26058d5f7b7cda03c8cd5f9c62bc3
AayushParashar28 <ayushparashar6203@gmail.com>
2023-01-07 23:34:21 +0530

    AnyBaseToDecimal

---
+++ AnyBaseToDecimal.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class AnyBaseToDecimal {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+    }
+}
