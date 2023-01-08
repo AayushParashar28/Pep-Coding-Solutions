52430a7b08c08ef7c9a0c7c8c3e6352ac496bb47
AayushParashar28 <ayushparashar6203@gmail.com>
2023-01-08 23:10:04 +0530

    AnyBaseToDecimal

--- AnyBaseToDecimal.java
+++ AnyBaseToDecimal.java
@@ -4,6 +4,17 @@ public class AnyBaseToDecimal {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int m = s.nextInt();
+        int b = s.nextInt();
+        int d = getValueIndecimal(n, b);
+        System.out.println(d);
     }
+
+
+
+public static int getValueIndecimal(int n, int b){
+    // write your code here
+    int ans ;
+
+    return ans ;
+ }
 }
